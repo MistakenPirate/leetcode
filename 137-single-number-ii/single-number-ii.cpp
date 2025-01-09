34766bc7239d9641abce50afb8ff00967b872576
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-01-09 22:30:00 +0530

    Time: 3 ms (34.21%) | Memory: 13.4 MB (54.94%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {        
+    int singleNumber(vector<int>& nums) {     
+        //better 2
         int n = nums.size();
-        int ans = 0;
+        sort(nums.begin(), nums.end());
 
-        for(int bitIndex = 0; bitIndex < 32; bitIndex++){
-            int count = 0;
-            for(int i = 0; i < n; i++)
-                //if the ith bit is set, increase count
-                if(nums[i] & (1 << bitIndex))
-                    count++;
-            
-            if(count % 3 == 1)
-                //set the ith bit of ans
-                ans = ans | (1 << bitIndex);
+        for(int i = 1; i < n; i += 3){
+            if(nums[i] != nums[i - 1])
+                return nums[i - 1];
         }
 
-        return ans;
+        return nums[n - 1];
+        
+        //better 1
+
+        //int n = nums.size();
+        //int ans = 0;
+
+        //for(int bitIndex = 0; bitIndex < 32; bitIndex++){
+        //    int count = 0;
+        //    for(int i = 0; i < n; i++)
+        //        //if the ith bit is set, increase count
+        //        if(nums[i] & (1 << bitIndex))
+        //            count++;
+            
+        //    if(count % 3 == 1)
+        //        //set the ith bit of ans
+        //        ans = ans | (1 << bitIndex);
+        //}
+
+        //return ans;
 
 
         //brute
