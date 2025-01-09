9cd29e10bc180aa5c83168d61c9968c3204be4be
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-01-09 22:25:29 +0530

    Time: 5 ms (6.68%) | Memory: 13.3 MB (54.94%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {        
-        unordered_map<int,int> mpp;
         int n = nums.size();
+        int ans = 0;
 
-        for(int i = 0; i < n; i++)
-            mpp[nums[i]]++;
+        for(int bitIndex = 0; bitIndex < 32; bitIndex++){
+            int count = 0;
+            for(int i = 0; i < n; i++)
+                //if the ith bit is set, increase count
+                if(nums[i] & (1 << bitIndex))
+                    count++;
+            
+            if(count % 3 == 1)
+                //set the ith bit of ans
+                ans = ans | (1 << bitIndex);
+        }
+
+        return ans;
+
+
+        //brute
         
-        for(auto it: mpp)
-            if(it.second == 1)
-                return it.first;
+        // unordered_map<int,int> mpp;
+        // int n = nums.size();
 
-        return -1;
+        // for(int i = 0; i < n; i++)
+        //     mpp[nums[i]]++;
+        
+        // for(auto it: mpp)
+        //     if(it.second == 1)
+        //         return it.first;
+
+        // return -1;
     }
 };
