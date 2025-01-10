8b96e94761500f80af871437513bdc93392ce525
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-01-10 10:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (54.39%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        vector<int> res;
+        //optimal
         int n = nums.size();
+        long XOR = 0;
 
         for(int i = 0; i < n; i++)
-            mpp[nums[i]]++;
+            XOR = XOR ^ nums[i];
+        
+        //getting the rightmost set bit
+        int rightmost = (XOR & (XOR - 1)) ^ XOR;
 
-        for(auto it: mpp){
-            if(it.second == 1)
-                res.push_back(it.first);
+        int XOR1 = 0, XOR2 = 0;
+
+        for(int i = 0; i < n; i++){
+            //divide numbers into buckets
+            //based on rightmost set bit
+            if(nums[i] & rightmost)
+                XOR1 = XOR1 ^ nums[i];
+            else 
+                XOR2 = XOR2 ^ nums[i];
         }
 
-        return res;
+        return {XOR1, XOR2};
+
+
+        //brute
+        // unordered_map<int,int> mpp;
+        // vector<int> res;
+        // int n = nums.size();
+
+        // for(int i = 0; i < n; i++)
+        //     mpp[nums[i]]++;
+
+        // for(auto it: mpp){
+        //     if(it.second == 1)
+        //         res.push_back(it.first);
+        // }
+
+        // return res;
     }
 };
