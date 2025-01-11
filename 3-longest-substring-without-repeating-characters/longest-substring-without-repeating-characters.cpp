01caae37a43c7fb610e5daeeba7b73d3f3f9d239
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-01-11 13:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (70.32%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //optimal
         int n = s.size();
-        int maxLen = 0;
 
-        for(int i = 0; i < n; i++){
+        //hash table to store last
+        //occurence of each char
+        vector<int> hash(256, -1);
+        
+        int l = 0, r = 0, maxLen = 0;
 
-            //hash for 256 characters ascii
-            vector<int> hash(256,0);
-
-            for(int j = i; j < n; j++){
-                //if it is in hash break
-                if(hash[s[j]] == 1) break;
-                
-                //if not put it in hash
-                hash[s[j]]++;
-                
-                //current length
-                int len = j - i + 1;
-                
-                maxLen = max(maxLen, len);
+        while(r < n){
+            //if current char s[r]
+            //is already in the substring
+            if(hash[s[r]] != -1){
+                //move left ptr to the
+                //right of the last 
+                //occurrence of s[r]
+                l = max(hash[s[r]] + 1, l);
             }
-        }     
+            int len = r - l + 1;
+            maxLen = max(len, maxLen);
+            
+            hash[s[r]] = r;
+            r++;
+        }
 
         return maxLen;
+
+        //brute
+        // int n = s.size();
+        // int maxLen = 0;
+
+        // for(int i = 0; i < n; i++){
+
+        //     //hash for 256 characters ascii
+        //     vector<int> hash(256,0);
+
+        //     for(int j = i; j < n; j++){
+        //         //if it is in hash break
+        //         if(hash[s[j]] == 1) break;
+                
+        //         //if not put it in hash
+        //         hash[s[j]]++;
+                
+        //         //current length
+        //         int len = j - i + 1;
+                
+        //         maxLen = max(maxLen, len);
+        //     }
+        // }     
+
+        // return maxLen;
     }
 };
